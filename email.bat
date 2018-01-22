c9195db065afde084778f620fa04097a8419e41d
FelisbertoCV <felisbertocv@gmail.com>
2018-01-22 22:08:39 +0000

    Update email.bat

--- email.bat
+++ email.bat
@@ -1,17 +1,17 @@
 @echo off
 title REBOOT_AP_PRO_UNIFI
-set ap_user=admin
-set ap_pass=S3A32037qZbo2Pkd
+set ap_user=username_do_terminal
+set ap_pass=passwor_do_terminal
 REM echo "Endereços dos APS Reniciados: " >> "C:\rebootunifi\relatorios\log_ap_reboot_%date%.txt"
 for /F %%a in (ip_aps.txt) do ( 	
-  REM c:\rebootunifi\plink admin@%%a -pw %ap_pass% "reboot"
+  REM c:\rebootunifi\plink %ap_user%@%%a -pw %ap_pass% "reboot"
   echo %%a >> "C:\rebootunifi\relatorios\log_ap_reboot_%date%.txt"
 )
 
-set subject="Reboot APS AvilaSec"
-set email_from=msn.networking@linkconsulting.com
-set email_to=felisberto.monteiro@linkconsulting.com
-set body="Os aps da infrastrutura AvilaSec foram reiniciados Consulte o endereço: https://psunificontroller.aitec.pt:8443, para verificar os seus estados."
+set subject="Reboot APS"
+set email_from=user@dominio.com
+set email_to=user@dominio.com
+set body="############Conteudo do e-mail"
 set server=psmail2013.aitec.pt
 blat.exe -attach "C:\rebootunifi\relatorios\log_ap_reboot_%date%.txt" -p configsmtp -to %email_to% -subject %subject% -body %body% -server %server% -f %email_from%
 pause
